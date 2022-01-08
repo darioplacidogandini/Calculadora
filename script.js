bcaa0616623eabd9a7cb0483d82b4b06433d4373
darioplacidogandini <darioplacidogandini@outlook.com>
2022-01-08 01:49:53 -0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function introducirNumero(valor) {
-    document.forms["pantalla"].value += valor;
+    if (document.forms["fPantalla"]["pantalla"].value == 0) {
+        document.forms["fPantalla"]["pantalla"].value = valor;  
+    } else {
+        document.forms["fPantalla"]["pantalla"].value += valor;
+    }
+}
+
+function borrar() {
+    document.forms["fPantalla"]["pantalla"].value = 0;
 }
