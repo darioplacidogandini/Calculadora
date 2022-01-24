b00deb940f699fdb4d8cfec3ce4e1cf655cff52f
darioplacidogandini <darioplacidogandini@outlook.com>
2022-01-24 19:52:09 -0300

    .

--- script.js
+++ script.js
@@ -33,6 +33,30 @@ function multiplicacion() {
     borrar();
 }
 
+function division() {
+    num1 = document.forms["fPantalla"]["pantalla"].value;
+    operador = "/";
+    borrar();   
+}
+
+function raizCuadrada() {
+    num1 = document.forms["fPantalla"]["pantalla"].value;
+    operador = "raizCuad";
+    borrar();     
+}
+
+function cubo() {
+    num1 = document.forms["fPantalla"]["pantalla"].value;
+    operador = "x^y";
+    borrar();
+}
+
+function fraccion() {
+    num1 = document.forms["fPantalla"]["pantalla"].value;
+    operador = "1/x";
+    borrar();
+}
+
 function mostrarResultado() {
     num2 = document.forms["fPantalla"]["pantalla"].value;    
     switch (operador) {
@@ -41,6 +65,14 @@ function mostrarResultado() {
         case "-": res = parseFloat(num1) - parseFloat(num2);
         break;
         case "*": res = parseFloat(num1) * parseFloat(num2);
+        break;
+        case "/": res = parseFloat(num1) / parseFloat(num2);
+        break;
+        case "raizCuad": res = Math.cbrt(num1);
+        break;
+        case "x^y": res = Math.pow(parseFloat(num1),parseFloat(num2));
+        break;
+        case "1/x": res = 1 / parseFloat(num1);
         break;
         default: res = "Operador invalido";
         break;
